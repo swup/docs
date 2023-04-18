3fc6ec3fe23954d57b16506ed31ee2095e8fadba
Rasso Hilber <mail@rassohilber.com>
2023-04-18 18:43:51 +0200

    Make `site.url` controllable via `env` var

---
+++ src/_data/site.js
@@ -0,0 +1,8 @@
+module.exports = function() {
+  return {
+	title: "swup",
+	description: "Complete, flexible, extensible, and easy-to-use page transition library for your server-side rendered website.",
+    url: process.env.SITE_URL || "development",
+	ga_tracking: "UA-65615068-6"
+  };
+};
