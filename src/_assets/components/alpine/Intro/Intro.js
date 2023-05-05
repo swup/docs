b743d44da76278c4b82aaef3a2b16d5d4a0675fa
Rasso Hilber <mail@rassohilber.com>
2023-05-05 11:34:28 +0200

    Add animation to front page logo

--- src/_assets/components/alpine/Intro/Intro.js
+++ src/_assets/components/alpine/Intro/Intro.js
@@ -1,8 +1,37 @@
 import { gsap } from "gsap";
 
 export default () => {
-	console.log(gsap);
+	const tl = gsap.timeline({delay: 0.2});
 	return {
+
+		init() {
+			const popIn = {
+				y: 50,
+
+				duration: 1.2,
+				autoAlpha: 0,
+				ease: "elastic.out"
+			}
+			tl.from(this.$refs.logo, {
+				y: 50,
+				scaleY: 3,
+				ease: "quint.out",
+				duration: 0.7
+			});
+			const letterDelay = "<0.1";
+			tl.from(this.$refs.s, popIn, 0);
+			tl.from(this.$refs.w, popIn, letterDelay);
+			tl.from(this.$refs.u, popIn, letterDelay);
+			tl.from(this.$refs.p, popIn, letterDelay);
+
+			tl.from(this.$refs.dot1, {...popIn, y: 20}, letterDelay);
+			tl.from(this.$refs.dot2, {...popIn, y: 20}, letterDelay);
+
+			const fadeIn = {...popIn, y: 20, ease: "quart.easeOut", duration: 0.4};
+			tl.from(this.$refs.slogan, fadeIn, "<0.5");
+			tl.from(this.$refs.buttons, fadeIn, letterDelay);
+			// tl.from(this.$refs.button2, fadeIn, letterDelay);
+		}
 
 	}
 }
