8967a44ad4c1242a040688e6c7dc2957b17583c9
Rasso Hilber <mail@rassohilber.com>
2023-05-09 09:43:44 +0200

    Add search modal

--- src/_assets/components/alpine/Search/Search.js
+++ src/_assets/components/alpine/Search/Search.js
@@ -2,9 +2,22 @@
 export default () => {
 
 	return {
+		isOpen: false,
 		metaKey: navigator.platform.indexOf('Mac') === 0 ? '⌘' : 'Ctrl+',
 		init() {
 
 		},
+		onKeyDown(e) {
+			if (e.key === '/') return this.open();
+			if (e.key === 'k' && (e.ctrlKey || e.metaKey)) {
+				this.open();
+			}
+		},
+		open() {
+			this.isOpen = true;
+		},
+		close() {
+			this.isOpen = false;
+		}
 	}
 }
