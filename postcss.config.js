62ea9b32b401015f332dcd290193bcff8f7c90de
Rasso Hilber <mail@rassohilber.com>
2023-05-12 11:43:20 +0200

    Uninstall `open-props`

--- postcss.config.js
+++ postcss.config.js
@@ -1,14 +1,7 @@
 const postcssPresetEnv = require('postcss-preset-env');
-const postcssJitProps = require('postcss-jit-props');
-const path = require('path');
 
 module.exports = {
 	plugins: [
-		postcssJitProps({
-			files: [
-				path.resolve(__dirname, 'node_modules/open-props/open-props.min.css'),
-			]
-		}),
 		postcssPresetEnv(),
 	]
 };
