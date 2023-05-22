8e3e22db9aa7925efe2a960b4460a4262b9659f1
Rasso Hilber <mail@rassohilber.com>
2023-05-22 22:28:35 +0200

    Optimizations for `TableOfContents.js`

--- eleventy.config.cjs
+++ eleventy.config.cjs
@@ -35,7 +35,7 @@ module.exports = function (eleventyConfig) {
 	eleventyConfig.addFilter('sortByOrder', sortByOrder);
 	eleventyConfig.addFilter('prepareContent', prepareContent);
 	eleventyConfig.addPlugin(eleventyNavigationPlugin);
-	eleventyConfig.addPlugin(tableOfContents, { tags: ['h2', 'h3'], wrapperClass: 'toc_nav' });
+	eleventyConfig.addPlugin(tableOfContents, { tags: ['h2', 'h3', 'h4'], wrapperClass: 'toc_nav' });
 	eleventyConfig.addFilter('getPreviousAndNextPage', getPreviousAndNextPage);
 	eleventyConfig.addShortcode('feather', renderFeatherIcon);
 	eleventyConfig.addShortcode('timestamp', () => Date.now());
--- src/_assets/components/alpine/TableOfContents/TableOfContents.js
+++ src/_assets/components/alpine/TableOfContents/TableOfContents.js
@@ -1,87 +1,59 @@
 export default () => {
-	let observer = null;
 	return {
-		windowWidth: 0,
-		windowHeight: 0,
+		wrap: null,
+		lastChild: null,
 		horizon: 150,
 		sections: [],
 		async init() {
 			await this.$nextTick();
+			this.wrap = document.querySelector('.page_body');
+			this.updateHorizon();
 			this.prepareSections();
-			this.onResize();
+			this.markCurrentSection();
 		},
 		bindings: {
-			'x-on:resize.window': 'onResize'
+			'x-on:resize.window': 'updateHorizon',
+			'x-on:scroll.window.throttle.10ms': 'markCurrentSection'
 		},
-		onResize() {
-			// Stop observing if the TOC is not visible
-			if (this.$root.getBoundingClientRect().width === 0) {
-				this.destroyObserver();
-				return;
-			}
-			if (this.windowDimensionsHaveChanged()) this.initObserver();
+		updateHorizon() {
+			this.horizon = this.$root.getBoundingClientRect().top + 30;
 		},
-		windowDimensionsHaveChanged() {
-			const widthChanged = this.windowWidth !== window.innerWidth;
-			const heightChanged = this.windowHeight !== window.innerHeight;
-			const hasChanged = widthChanged || heightChanged;
-			this.windowWidth = window.innerWidth;
-			this.windowHeight = window.innerHeight;
-			return hasChanged;
-		},
+		// Store a reference to all sections in reversed order
 		prepareSections() {
 			this.sections = [
-				...document.querySelector('.page_body').querySelectorAll('h2,h3')
+				...this.wrap.querySelectorAll('h2[id],h3[id],h4[id]')
 			].reverse();
 		},
-
-		initObserver() {
-			this.horizon = this.$root.getBoundingClientRect().top + 100;
-
-			if (observer) observer.disconnect();
-
-			const top = -this.horizon;
-			const bottom = -window.innerHeight + this.horizon;
-			const options = {
-				rootMargin: `${top}px 0px ${bottom}px 0px`
-			};
-
-			observer = new IntersectionObserver(() => this.updateCurrentSection(), options);
-
-			this.sections.forEach((section) => {
-				if (observer) observer.observe(section);
-			});
-		},
 		/**
-		 * Runs every time the intersectionObserver callback is being fired.
-		 *
-		 * This is the best compromise between performance and flexibility
-		 *
-		 * Tries to find the closest section that is above `this.horizon`.
-		 * If no section could be found, falls back to the first section
-		 *
+		 * Marks the current section with the class "is-active"
 		 * @returns void
 		 */
-		updateCurrentSection() {
-			const currentSection =
-				this.sections.find((section) => {
-					const rect = section.getBoundingClientRect();
-					// ignore invisible sections
-					if (!rect.height) return false;
-					return rect.top < this.horizon;
-				}) || this.sections[this.sections.length - 1];
-
+		markCurrentSection() {
+			const currentSection = this.getCurrentSection();
 			this.$root.querySelector('a.is-active')?.classList.remove('is-active');
 			this.$root.querySelector(`a[href="#${currentSection.id}"]`)?.classList.add('is-active');
 		},
-		destroyObserver() {
-			if (observer) {
-				observer.disconnect();
-				observer = null;
+		/**
+		 * Tries to find the current section. The sections are reversed (from bottom to top), to
+		 * make the whole process more performant.
+		 * @returns HTMLElement
+		 */
+		getCurrentSection() {
+			const firstSection = this.sections[this.sections.length - 1];
+			const lastSection = this.sections[0];
+
+			// If the bottom of the document is reached, mark the last section as the active one
+			if (window.scrollY >= document.documentElement.offsetHeight - window.innerHeight - 5) {
+				return lastSection;
 			}
-		},
-		destroy() {
-			this.destroyObserver();
+			// Find the first section that is above the horizon.
+			// If no section can be found, return the first section
+			return this.sections.find((section) => {
+				const rect = section.getBoundingClientRect();
+				// ignore invisible sections
+				if (!rect.height) return false;
+				return rect.top < this.horizon;
+			}) || firstSection;
 		}
 	};
 };
