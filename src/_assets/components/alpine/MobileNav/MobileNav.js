36c1ac67efed606521ab75b6d0a60a23e2d7d073
Rasso Hilber <mail@rassohilber.com>
2023-05-22 13:04:58 +0200

    Cleanup

--- src/_assets/components/alpine/MobileNav/MobileNav.js
+++ src/_assets/components/alpine/MobileNav/MobileNav.js
@@ -5,9 +5,6 @@ export default () => {
 		init() {
 			this.navHTML = document.querySelector('.nav').innerHTML;
 		},
-		onKeyDown(e) {
-			if (e.key === '/') return this.open();
-		},
 		open() {
 			this.isOpen = true;
 		},
