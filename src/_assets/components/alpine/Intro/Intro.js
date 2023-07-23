4be441d63d6c5a1f5b52a8213e4310aea1e0fcba
Philipp Daun <post@philippdaun.net>
2023-07-23 16:03:38 +0200

    Allow fast-forwarding intro animation

--- src/_assets/components/alpine/Intro/Intro.js
+++ src/_assets/components/alpine/Intro/Intro.js
@@ -2,6 +2,33 @@ import { gsap } from 'gsap';
 
 export default () => {
 	const tl = gsap.timeline({ delay: 0.5 });
+
+	const popIn = {
+		y: 50,
+		duration: 0.4,
+		autoAlpha: 0,
+		ease: 'back.out',
+		clearProps: 'all',
+		transition: 'none'
+	};
+
+	const fadeIn = {
+		autoAlpha: 0,
+		y: 20,
+		ease: 'quart.easeOut',
+		duration: 0.4,
+		clearProps: 'all'
+	};
+
+	const slideUp = {
+		y: 50,
+		scaleY: 3,
+		ease: 'quint.out',
+		duration: 0.7
+	};
+
+	const letterDelay = '<0.1';
+
 	return {
 		async init() {
 			this.$root.style.visibility = 'hidden';
@@ -12,23 +39,12 @@ export default () => {
 
 			this.$root.style.visibility = 'visible';
 
-			const popIn = {
-				y: 50,
-				duration: 0.4,
-				autoAlpha: 0,
-				ease: 'back.out',
-				clearProps: 'all',
-				transition: 'none'
-			};
+			this.createAnimation();
+		},
 
-			tl.from(this.$refs.link, {
-				y: 50,
-				scaleY: 3,
-				ease: 'quint.out',
-				duration: 0.7
-			});
+		createAnimation() {
+			tl.from(this.$refs.link, slideUp);
 
-			const letterDelay = '<0.1';
 			tl.from(this.$refs.s, popIn, 0);
 			tl.from(this.$refs.w, popIn, letterDelay);
 			tl.from(this.$refs.u, popIn, letterDelay);
@@ -37,19 +53,17 @@ export default () => {
 			tl.from(this.$refs.dot1, { ...popIn, y: -20 }, '<0.3');
 			tl.from(this.$refs.dot2, { ...popIn, y: -20 }, letterDelay);
 
-			const fadeIn = {
-				autoAlpha: 0,
-				y: 20,
-				ease: 'quart.easeOut',
-				duration: 0.4,
-				clearProps: 'all'
-			};
+
 			tl.from(this.$refs.slogan, fadeIn, '<0.3');
 
 			tl.from(this.$refs.button1, fadeIn, letterDelay);
 			tl.from(this.$refs.button2, fadeIn, letterDelay);
 
 			tl.from(this.$refs.announcement, { ...popIn, y: -20 }, '<0.5');
+		},
+
+		cancelAnimation() {
+			tl.seek(5);
 		},
 
 		sliceLogo() {
