b3f85a172dc0efd072fa810f3b39b97cfdcea4c4
Rasso Hilber <mail@rassohilber.com>
2023-07-24 17:04:24 +0200

    Update meta

--- src/_data/site.js
+++ src/_data/site.js
@@ -1,8 +1,8 @@
 module.exports = function () {
 	return {
-		title: 'swup 3 docs archive',
+		title: 'Docs Archive for swup 3',
 		description:
-			'Archived documentation for swup v3',
+			'Versatile and extensible page transition library for server-rendered websites',
 		url: process.env.SITE_URL || 'https://v3.swup.js.org',
 		ga_tracking: 'UA-65615068-6'
 	};
