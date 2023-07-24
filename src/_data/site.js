b5243b770e9ee131bc5b7882d7213937fe500aa0
Rasso Hilber <mail@rassohilber.com>
2023-07-24 17:56:52 +0200

    Add version select

--- src/_data/site.js
+++ src/_data/site.js
@@ -4,6 +4,18 @@ module.exports = function () {
 		description:
 			'Versatile and extensible page transition library for server-rendered websites',
 		url: process.env.SITE_URL || 'https://swup.js.org',
-		ga_tracking: 'UA-65615068-6'
+		ga_tracking: 'UA-65615068-6',
+		currentVersion: 'v4',
+		versions: [
+			{
+				label: 'swup 4 (current)',
+				url: process.env.SITE_URL || 'https://swup.js.org',
+				isCurrent: true
+			},
+			{
+				label: 'swup 3',
+				url: 'https://v3.swup.js.org',
+			},
+		]
 	};
 };
