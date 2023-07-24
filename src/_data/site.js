515a77ab857efc465fb9945152df3c684e4335a1
Rasso Hilber <mail@rassohilber.com>
2023-07-24 17:06:27 +0200

    replace invisible character with space

--- src/_data/site.js
+++ src/_data/site.js
@@ -2,7 +2,7 @@ module.exports = function () {
 	return {
 		title: 'swup',
 		description:
-			'Versatile and extensible page transition library for server-rendered websites',
+			'Versatile and extensible page transition library for server-rendered websites',
 		url: process.env.SITE_URL || 'https://swup.js.org',
 		ga_tracking: 'UA-65615068-6'
 	};
